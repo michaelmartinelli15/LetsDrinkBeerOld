afcd666ca2e46a84d7bd7791ec3c24446dcd4a67
Michael Martinelli <martinellimichael15@gmail.com>
2021-04-27 11:58:44 -0700

    Updates

--- src/components/DegeneracyScreen.tsx
+++
@@ -1,5 +0,0 @@
-import { useDegeneracyComponent, DegeneracyProps } from "../utils/useDegeneracyComponent";
-
-export const DegeneracyScreen = ({ degeneracyLevel }: DegeneracyProps): JSX.Element => {
-    return useDegeneracyComponent(degeneracyLevel);
-};
--- src/interfaces/IBeer.tsx
+++
@@ -1,8 +0,0 @@
-export interface Beer {
-    Name: string;
-    Container: string;
-    Ounces: number;
-    BoxSize: number;
-    BeersLeft: number;
-    TotalDrank: number;
-}
